00c20bae4a04aa8f472b891e894bff1ac1d963e5
gencaypolat <gencaypolat@gmail.com>
2024-03-05 17:08:03 +0300

    Route Parameters added

--- RoutingExample/Program.cs
+++ RoutingExample/Program.cs
@@ -7,6 +7,26 @@ app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     //add your end point
+
+    endpoints.Map("files/{filename}.{extension}", async context =>
+    {
+        string? fileName = Convert.ToString(context.Request.RouteValues["filename"]);
+        string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
+
+        await context.Response.WriteAsync($"In files - {fileName}.{extension}");
+    });
+
+    endpoints.Map("employee/profile/{EmployeeName}", async (context) =>
+    {
+        string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
+
+        await context.Response.WriteAsync($"In Employee profile - {employeeName}");
+    });
+});
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync($"Request recieved at / {context.Request.Path}");
 });
 
 app.Run();
